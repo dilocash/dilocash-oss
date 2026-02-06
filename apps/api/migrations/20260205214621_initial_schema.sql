815be53b46a3177a3ca778a6c2b51de3ecba8765
Jonathan Albarran <jaal1388@gmail.com>
2026-02-06 13:03:13 -0400

    license applied

--- apps/api/migrations/20260205214621_initial_schema.sql
+++ apps/api/migrations/20260205214621_initial_schema.sql
@@ -1,3 +1,7 @@
+-- Copyright (c) 2026 dilocash
+-- Use of this source code is governed by an MIT-style
+-- license that can be found in the LICENSE file.
+
 -- Create "users" table
 CREATE TABLE "public"."users" (
   "id" uuid NOT NULL,
