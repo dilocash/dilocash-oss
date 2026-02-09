feefab7dd375eebc542504d8dda4f8aa863a0b3b
Jonathan Albarran <jaal1388@gmail.com>
2026-02-09 09:05:15 -0400

    base web implementation

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,29 +1,44 @@
 import { AuthButton } from "@/components/auth-button";
-import Image from "next/image";
 import { Suspense } from "react";
 import { ThemeSwitcher } from "@/components/theme-switcher";
+import { Link } from "lucide-react";
+import { Hero } from "@/components/hero";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/dilocash.svg"
-          alt="dilocash logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <Suspense fallback={<p>Loading...</p>}>
-                <AuthButton />
-              </Suspense>
+    <main className="min-h-screen flex flex-col items-center">
+      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+            <Suspense fallback={<div>Loading...</div>}>
+              <AuthButton />
+              <ThemeSwitcher></ThemeSwitcher>
+            </Suspense>
+          </div>
+        </nav>
+        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
+          <Hero />
+          <main className="flex-1 flex flex-col gap-6 px-4">
+            <h2 className="font-medium text-xl mb-4">Next steps</h2>
+            <span>Register or login</span>
+          </main>
         </div>
+
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+          <p>
+            Powered by{" "}
+            <a
+              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+              target="_blank"
+              className="font-bold hover:underline"
+              rel="noreferrer"
+            >
+              Supabase
+            </a>
+          </p>
           <ThemeSwitcher />
         </footer>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
--- apps/web/components/auth-button.tsx
+++ apps/web/components/auth-button.tsx
@@ -21,7 +21,6 @@ export async function AuthButton() {
       <Button asChild size="sm" variant={"outline"}>
         <Link href="/auth/login">Sign in</Link>
       </Button>
-      <div className="text-red-900">a div</div>
       <Button asChild size="sm" variant={"default"}>
         <Link href="/auth/sign-up">Sign up</Link>
       </Button>
