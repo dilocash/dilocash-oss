4b9a0b14d0efd317de6ee8da0deb9ba2ba1d3eee
Jonathan Albarran <jaal1388@gmail.com>
2026-02-10 08:44:31 -0400

    feature: supabase user registration, login and password reset

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,6 +9,9 @@ export default function Home() {
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+            <div className="flex gap-5 items-center font-semibold">
+              <span>Dilocash</span>
+            </div>
             <Suspense fallback={<div>Loading...</div>}>
               <AuthButton />
             </Suspense>
@@ -16,10 +19,6 @@ export default function Home() {
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-            <span>Register or login</span>
-          </main>
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
