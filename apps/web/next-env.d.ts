83ea9abd76be4d802f612a7fafe8da757d8a238a
Jonathan Albarran <jaal1388@gmail.com>
2026-02-10 09:36:21 -0400

    license check

--- apps/web/next-env.d.ts
+++ apps/web/next-env.d.ts
@@ -1,9 +1,3 @@
-/**
- * Copyright (c) 2026 dilocash
- * Use of this source code is governed by an MIT-style
- * license that can be found in the LICENSE file.
- */
-
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 import "./.next/dev/types/routes.d.ts";
