bc1545843b79c9916bbff10d0bb9ce1035decb1a
Jonathan Albarran <jaal1388@gmail.com>
2026-02-15 19:29:28 -0400

    i18n web implementation

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,15 +1,26 @@
 "use client";
-import "../styles/global.css"; 
+import "../styles/global.css";
 
 import { GluestackUIProvider } from "@dilocash/ui/components/ui/gluestack-ui-provider";
+import i18n, { initI18n } from '@dilocash/i18n';
+import { useEffect, useState } from 'react';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isReady, setIsReady] = useState(false);
+  useEffect(() => {
+    // Inicializamos indicando que es Web para activar el detector
+    initI18n(true).then(() => setIsReady(true));
+  }, []);
+  if (!isReady) return <html>
+      <body>
+      </body>
+    </html>; // Or a loading spinner
   return (
-    <html lang="en">
+    <html lang={i18n.language}>
       <body>
         <GluestackUIProvider mode="light">
           {children}
