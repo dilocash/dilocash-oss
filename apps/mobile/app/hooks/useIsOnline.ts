bfdf64eed73fccd67f060cd6f4f98354a7042322
Jonathan Albarran <jaal1388@gmail.com>
2026-02-22 14:23:10 -0400

    improved expo dependencies versions

--- apps/mobile/app/hooks/useIsOnline.ts
+++ apps/mobile/app/hooks/useIsOnline.ts
@@ -1,12 +1,12 @@
-import { useState, useEffect } from 'react';
-import NetInfo from '@react-native-community/netinfo';
+import { useState, useEffect } from "react";
+import NetInfo from "@react-native-community/netinfo";
 
 export const useIsOnline = () => {
   const [isOnline, setIsOnline] = useState(true);
 
   useEffect(() => {
-    // Suscribirse a los cambios de red
-    const unsubscribe = NetInfo.addEventListener(state => {
+    // Subscribe to network changes
+    const unsubscribe = NetInfo.addEventListener((state) => {
       setIsOnline(!!state.isConnected && !!state.isInternetReachable);
     });
 
